f7aee30264cecb045bb28b516f47d74983c9638f
zhufeng <zhfjyq@163.com>
2018-04-08 15:33:28 +0800

    OK

---
+++ test/cmdline.ts
@@ -0,0 +1,31 @@
+import argv from '../src/argv';
+import pkg from '../package.json';
+
+/**
+ * 导出解析后的命令行
+ */
+export default argv(
+  pkg.name,
+  pkg.version,
+  `  Example using for Test ts argv library
+  you can using: "${pkg.name} [cmd] params ..." to test it
+  `,
+  {
+    cmd: {
+      short: null,
+      desc: 'config directory ,default is ./config',
+      value: 'run', // 缺省值
+    },
+    port: {
+      short: 'p',
+      desc: 'service listen port,default:3000',
+      value: 3000, // 缺省值
+    },
+    boo: {
+      short: 'b',
+      desc: 'service listen port,default:3000',
+      value: false, // 缺省值      
+    }
+  }
+);
+
---
+++ test/index.ts
@@ -0,0 +1,6 @@
+
+import cmdline from './cmdline';
+
+console.log('cmd', cmdline.cmd);
+console.log('port', cmdline.port, typeof (cmdline.port));
+console.log('boo', cmdline.boo, typeof (cmdline.boo));
